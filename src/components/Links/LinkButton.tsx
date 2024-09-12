2ec1d8359a2fc0dea4a47f20e255fafda927a6bf
John Wesley Baik <johnwbaik@gmail.com>
2024-09-12 17:22:56 +0900

    Update LinkButton.tsx

--- src/components/Links/LinkButton.tsx
+++ src/components/Links/LinkButton.tsx
@@ -2,7 +2,7 @@ import type { ReactNode } from "react";
 
 export default function LinkButton(props: { children: ReactNode }) {
   return (
-    <button className="w-full font-inter-bold text-[#FFFFFF] py-4 rounded-lg bg-[#333333]">
+    <button className="w-full font-inter-bold hover:bg-[#C4F829] hover:text-[#002100] text-[#FFFFFF] py-4 rounded-lg bg-[#333333]">
       {props.children}
     </button>
   );
